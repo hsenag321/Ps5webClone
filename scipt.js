d70b3b274e9462e62d9872b0474e7095bd574296
Ganesh_kandel1 <151698628+hsenag321@users.noreply.github.com>
2025-04-13 23:40:54 +0930

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,100 @@
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+let third = document.getElementById("third");
+let fourth = document.getElementById("fourth");
+let fifth = document.getElementById("fifth");
+let sixth = document.getElementById("sixth");
+let seventh = document.getElementById("seventh");
+let optionsps5 = document.querySelector(".options_ps5")
+
+first.addEventListener("click",()=>{
+    optionsps5.innerHTML= ` <div class="ps5_console">
+                        <p class="ps5_des"> Discover all PS5 consoles and accessories </p>
+                        <h3 id="title">PlayStation 5 Console</h3>
+                        <p class="extra"> Experience of all-new generation of incredible playstation games.</p>
+                        <button class="sign_in"> Find out more </button>
+                        <button class="buy"> Buy now </button>
+                        </div>
+                        <div class="ps5image">
+                        <img src="ps5console.webp">
+                        </div>`
+})
+
+second.addEventListener("click",()=>{
+    optionsps5.innerHTML= ` <div class="ps5_console">
+                        <p class="ps5_des"> Discover all PS5 consoles and accessories </p>
+                        <h3 id="title">PlayStation 5 pro Console</h3>
+                        <p class="extra"> Play PS5® games with the most impressive visuals ever possible on a PlayStation console..</p>
+                        <button class="sign_in"> Find out more </button>
+                        <button class="buy"> Buy now </button>
+                        </div>
+                        <div class="ps5image">
+                        <img src="ps5 pro.webp">
+                        </div>`
+})
+
+third.addEventListener("click",()=>{
+    optionsps5.innerHTML= ` <div class="ps5_console">
+                        <p class="ps5_des"> Discover all PS5 consoles and accessories </p>
+                        <h3 id="title">DualSense™ Wireless Controller</h3>
+                        <p class="extra">Immerse yourself in the gaming experience with a controller the supports responsive haptic feedback and dynamic trigger effects </p>
+                        <button class="sign_in"> Find out more </button>
+                        <button class="buy"> Buy now </button>
+                        </div>
+                        <div class="ps5image">
+                        <img src="dualsense.webp">
+                        </div>`
+})
+
+fourth.addEventListener("click",()=>{
+    optionsps5.innerHTML= ` <div class="ps5_console">
+                        <p class="ps5_des"> Discover all PS5 consoles and accessories </p>
+                        <h3 id="title">PULSE Elite™ Wireless Headset</h3>
+                        <p class="extra">Enjoy lifelike gaming audio in a comfortable headset design equipped with a retractable microphone and built-in long-life battery</p>
+                        <button class="sign_in"> Find out more </button>
+                        <button class="buy"> Buy now </button>
+                        </div>
+                        <div class="ps5image">
+                        <img src="PULSEEliteheadset.webp">
+                        </div>`
+})
+
+fifth.addEventListener("click",()=>{
+    optionsps5.innerHTML= ` <div class="ps5_console">
+                        <p class="ps5_des"> Discover all PS5 consoles and accessories </p>
+                        <h3 id="title">PULSE Explore™ Wireless Earbuds</h3>
+                        <p class="extra"> Enjoy lifelike gaming audio wherever play takes you with a portable design equipped with hidden microphones and a companion charging case.</p>
+                        <button class="sign_in"> Find out more </button>
+                        <button class="buy"> Buy now </button>
+                        </div>
+                        <div class="ps5image">
+                        <img src="PULSEExploreearbuds.webp">
+                        </div>`
+})
+
+sixth.addEventListener("click",()=>{
+    optionsps5.innerHTML= ` <div class="ps5_console">
+                        <p class="ps5_des"> Discover all PS5 consoles and accessories </p>
+                        <h3 id="title">DualSense Edge™ Wireless Controller</h3>
+                        <p class="extra">Get an edge in gameplay with remappable buttons, tuneable triggers and sticks, changeable stick caps, back buttons, and more .</p>
+                        <button class="sign_in"> Find out more </button>
+                        <button class="buy"> Buy now </button>
+                        </div>
+                        <div class="ps5image">
+                        <img src="dualsense-edge.webp">
+                        </div>`
+})
+
+seventh.addEventListener("click",()=>{
+    optionsps5.innerHTML= ` <div class="ps5_console">
+                        <p class="ps5_des"> Discover all PS5 consoles and accessories </p>
+                        <h3 id="title">Access™ Controller</h3>
+                        <p class="extra"> A highly customisable PlayStation®5 controller kit designed to make gaming more accessible.</p>
+                        <button class="sign_in"> Find out more </button>
+                        <button class="buy"> Buy now </button>
+                        </div>
+                        <div class="ps5image">
+                        <img src="access-controller.webp">
+                        </div>`
+})
+
